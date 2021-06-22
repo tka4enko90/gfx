03689104949ac8d590356f895ac3d6a069de6c2d
mtelniy <mtelniy@markupus.tech>
2021-06-22 18:05:46 +0300

    updates from client (animation for elements in hero section / show submenu on hover)

--- wp-content/themes/gfx/src/js/partials/app.js
+++ wp-content/themes/gfx/src/js/partials/app.js
@@ -1,6 +1,5 @@
 (function ($) {
     'use strict';
-
     var body = $('body');
 
     // add sticky class for header after scroll
@@ -17,59 +16,52 @@
         }
     });
 
-    // preventDefault for not a link link menu
+    // preventDefault for not a link items in menu
     $('.not-a-link > a').on('click', function (e) {
         e.preventDefault();
     });
 
-    // show submenu on click
-    var hasChildrenLink = $('.header li.menu-item-has-children > a');
-    if (hasChildrenLink.length) {
-        hasChildrenLink.on('click', function (e) {
-            e.preventDefault();
+    // show submenu on hover/click
+    var hasChildrenListItem = $('.header .menu > li.menu-item-has-children');
+    var hasChildrenLink = hasChildrenListItem.find('a:first');
 
-            var subMenu = $(this).next('.sub-menu');
-            var subMenus = $('.header .menu > li.menu-item-has-children > .sub-menu');
+    if ($(window).width() > 960) {
+        if (hasChildrenListItem.length) {
+            hasChildrenListItem.hover(function () {
 
-            if ($(window).width() > 960) {
+                var subMenu = $(this).find('a:first').next('.sub-menu');
+                var subMenus = $('.header .menu > li.menu-item-has-children > .sub-menu');
+
                 if (subMenus.length) {
                     subMenus.fadeOut(100);
-                    hasChildrenLink.removeClass('active');
-                }
+                    hasChildrenListItem.removeClass('active');
 
-                if (subMenu.length) {
                     if (!subMenu.is(':visible')) {
                         $(this).addClass('active');
                         subMenu.fadeIn(200).css('display', 'flex');
                     }
                 }
-            } else {
-                if (subMenus.length) {
+            });
+        }
+    } else {
+        if (hasChildrenListItem.length) {
+            hasChildrenListItem.on('click', function () {
+                var link = $(this).find('a:first');
+                var subMenu = link.next('.sub-menu');
+                var subMenus = $('.header .menu > li.menu-item-has-children > .sub-menu');
+
+                if (subMenus.length && hasChildrenLink.length) {
                     subMenus.slideUp(200);
                     hasChildrenLink.removeClass('opened');
                 }
 
-                if (subMenu.length && !subMenu.is(':visible')) {
-                    $(this).addClass('opened');
+                if (subMenu.length && !subMenu.is(':visible') && link.length) {
+                    link.addClass('opened');
                     subMenu.slideDown(200).css('display', 'flex');
                 }
-            }
-        });
-    }
-
-
-    // close sub-menu on click not in sub-menus area
-    $(document).mouseup(function (e) {
-        if ($(window).width() > 960) {
-            var links = $('.header .menu-holder > ul > li.menu-item-has-children > a');
-            var submenu = links.next('.sub-menu');
-            if (!submenu.is(e.target)
-                && submenu.has(e.target).length === 0) {
-                links.removeClass('active');
-                submenu.fadeOut(100);
-            }
+            });
         }
-    });
+    }
 
     // show mobile menu on click
     $('.header .burger-btn').on('click', function () {
@@ -87,7 +79,7 @@
 
     // show added to cart popup
     var addedToCartPopup = $('.product-added-to-cart-popup');
-    if(body.length) {
+    if (body.length) {
         body.on('added_to_cart', function (event, fragments, cart_hash, button) {
             var addedProductName = button.attr('data-product_title');
             var productPopOutCloseBtn = $('.product-pop-out-close-btn');
@@ -112,10 +104,6 @@
             }
         });
     }
-
-    $( body ).on( 'updated_cart_totals', function(){
-        //re-do your jquery
-    });
 
     // close added to cart popup
     $('.product-added-to-cart-close-btn').on('click', function () {
