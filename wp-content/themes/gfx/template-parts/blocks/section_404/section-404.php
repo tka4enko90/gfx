810b4632d0bcdf58c9215d9af8bc55324bceadf7
mtelniy <mtelniy@markupus.tech>
2021-07-07 18:28:41 +0300

    feat: 404 (1-2h) | develop 404 page | https://markupus.kaiten.io/space/32418/card/2545199

---
+++ wp-content/themes/gfx/template-parts/blocks/section_404/section-404.php
@@ -0,0 +1,23 @@
+<?php wp_enqueue_style('section_404_css', get_template_directory_uri() . '/static/css/template-parts/blocks/section_404/section-404.css', '', '', 'all'); ?>
+
+<?php
+$page_title_404 = get_field('page_title_404', 'option');
+$page_subtitle_404 = get_field('page_subtitle_404', 'option');
+?>
+
+<section class="section-404">
+    <div class="container small">
+        <div class="section-holder">
+            <h1>404</h1>
+            <?php if ($page_title_404) : ?>
+                <h2><?php echo $page_title_404; ?></h2>
+            <?php endif; ?>
+            <?php if ($page_subtitle_404) : ?>
+                <div class="subtitle"><?php echo $page_subtitle_404; ?></div>
+            <?php endif; ?>
+            <div class="btn-holder">
+                <a href="<?php echo home_url(); ?>" class="primary-button">Back to Homepage</a>
+            </div>
+        </div>
+    </div>
+</section>
