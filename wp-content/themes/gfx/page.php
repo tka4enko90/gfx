fb57781ef327090d24caa704d2a5efffc6b8fe97
Maksim Telniy <mtelniy@markupus.tech>
2021-09-01 16:56:06 +0300

    fix: Press page | fix page.php | https://markupus.kaiten.io/space/32418/card/2717922

--- wp-content/themes/gfx/page.php
+++ wp-content/themes/gfx/page.php
@@ -6,17 +6,12 @@
 get_header(); ?>
 
     <main class="main">
-        <section>
-            <div class="container">
-                <?php
-                while (have_posts()) :
-                    the_post();
-
-                    the_content();
-                endwhile;
-                ?>
-            </div>
-        </section>
+        <?php
+        while (have_posts()) :
+            the_post();
+            the_content();
+        endwhile;
+        ?>
     </main>
 
 <?php get_footer(); ?>
