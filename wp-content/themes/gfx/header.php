d845515b24aeab723fffb01a39766cfc87bc2e5a
Alexander Tkachenko <sasha@markupus.tech>
2022-01-12 19:07:08 +0200

    updated: updated favicon

--- wp-content/themes/gfx/header.php
+++ wp-content/themes/gfx/header.php
@@ -18,7 +18,12 @@
     <title><?php bloginfo('name'); ?> <?php wp_title("", true); ?></title>
 
     <!-- FAVICON -->
-    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/static/img/favicon.ico" type="image/x-icon"/>
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/<?php echo get_template_directory_uri(); ?>/faviconsite.webmanifest">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
     <!-- /FAVICON -->
 
     <?php wp_head() ?>
